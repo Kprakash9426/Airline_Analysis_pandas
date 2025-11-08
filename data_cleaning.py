a41a987aa4310f8ced5b6cb77115cfa938f61742
Kprakash9426 <kprakasheee35@gmail.com>
2025-11-08 10:05:10 +0530

    data loading

---
+++ data_cleaning.py
@@ -0,0 +1,39 @@
+# Importing Required Modules
+# importing numpy for mathematical operation on arrays and dataframe.
+
+# importing pandas for reading data and data manipualtion.
+
+# importing matplotlib and seaborn to show the insights and visualization from the dataset.
+
+# importing warnings for Warning messages that are typically issued in dataframe where it is useful to alert the user of some condition in a program, where that condition (normally) doesn t warrant raising an exception and terminating the program.
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import warnings
+warnings.filterwarnings("ignore")
+
+# Reading Dataset and Checking the NaN Values , Data Types , and Statistical Analysis
+# Since data is in form of excel file we have to use pandas read_excel to load the data
+
+# After loading it is important to check the complete information of data as it can indication many of the hidden infomation such as null values in a column or a row
+
+# Check whether any null values are there or not. if it is present then following can be done,
+
+# Filling NaN values with mean, median and mode using fillna() method Describe data --> which can give statistical analysis
+
+df=pd.read_excel("Data_Train.xlsx")  #Loading excel data
+print(df.head()) # Reading the top  5 rows
+
+print(df.info()) #it is useful to get data information like no of rows and colums,non null count,datatype of the columns ,memory used
+
+print("---It is useful to get the statstical information about the dataset----")
+df.describe()
+df.describe(include=object)
+df.describe(include=float)
+print(df.describe(include=object))
+
+#to check null values
+print("----------displaying the null values count --------")
+print(df.isnull().sum())
